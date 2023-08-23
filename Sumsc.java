1c809d423ee890773cb87fac59e0edd9c4f1f25c
Paresh4577 <142432711+Paresh4577@users.noreply.github.com>
2023-08-23 20:19:05 +0530

    Add files via upload

---
+++ Sumsc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sumsc{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter 1 Num:");
+		int a = sc.nextInt();
+
+		System.out.print("Enter 2 Num:");
+		int b = sc.nextInt();
+
+		System.out.println("Sum is:"+(a+b));
+	}
+}
